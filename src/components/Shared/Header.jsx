df0835c03de8e8278a8ab6a74faba8b594837b64
cerberus8888 <v26396341@gmail.com>
2023-03-02 22:05:19 -0500

    update

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -11,29 +11,7 @@ export const Header = () => {
             <ul className='navbar__list'>
                 <li><Link to='/user/login'><i className="fal fa-user"></i></Link></li>
                 <li><Link to='/purchases'><i className="fal fa-archive"></i></Link></li>
-                {
-                  localStorage.getItem('token') ? 
-                    <>
-                      <li data-bs-toggle="offcanvas" data-bs-target="#cartModal" aria-controls="cartModal">
-                        <i className="fal fa-shopping-cart"></i>
-                      </li>
-                      <div className="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="cartModal" aria-labelledby="staticBackdropLabel">
-                        <div className="offcanvas-header">
-                          <h5 className="offcanvas-title" id="offcanvasRightLabel">
-                            <div className='gotoCartLink-container'>
-                              <Link to='/cart' className='gotoCartLink'>Go to cart</Link>
-                            </div>
-                          </h5>
-                          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div className="offcanvas-body overflow-auto">
-                          <CartPage />
-                        </div>
-                      </div>
-                    </>
-                    : 
-                    <li><Link to='/user/login'><i className="fal fa-shopping-cart"></i></Link></li>
-                }
+                <li><Link to='/cart'><i className="fal fa-shopping-cart"></i></Link></li>
             </ul>
         </nav>
     </header>
